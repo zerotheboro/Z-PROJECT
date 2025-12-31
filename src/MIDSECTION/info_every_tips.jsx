c96d4659cf32a129a36d4624160ff9dfa663e0af
zerotheboro <2002070343@royal.edu.vn>
2025-12-31 20:34:27 +0700

    update abt the structure

--- src/MIDSECTION/info_every_tips.jsx
+++ src/MIDSECTION/info_every_tips.jsx
@@ -73,13 +73,66 @@ class DetailOfTips{
 export const list_of_tips = [
   new TypeOfTips("PRE-LEARN", [
     new DetailOfTips("Prime mind", "asking questions like why must I?...", image_importor("brain.svg")),
-    new DetailOfTips("WATER ur face/body", "a little shift of % can affect ur brain alot!", image_importor("water")),
-    new DetailOfTips("BREAK ≠ DISTRACTION", "don't pull out ur phone cause distraction unlike break it hooks u up and take more uneccsary time", image_importor("relaxing_cat")),
-    new DetailOfTips("small workout", "jumpstart ur blood, get the oxygen ur brain needs", image_importor("dumbell")),
-    new DetailOfTips("Structure your day", "brain loves certainty, structuring reduces energy of deciding what to do next", image_importor("multiple_checkbox")),
-    new DetailOfTips("Track progress", "motivate you and keep you in mind how much you progress", image_importor("checkbox")),
-    new DetailOfTips("AUTO GOOGLE", "This feature conditions u into going to the tab you want to learn rigth away; there is a set up tutorial", image_importor("google", "video"))
+    new DetailOfTips("WATER ur face/body", 
+    <>
+      Drink water around a glass of water
+      and splash water onto ur face
+    </>,
+    image_importor("water")),
+
+    
+    new DetailOfTips("BREAK ≠ DISTRACTION", 
+    <>
+      don't pull out ur phone cause distraction
+      unlike break it hooks u up and take more uneccsary time
+    </>,
+    image_importor("relaxing_cat")),
+
+
+    new DetailOfTips("small workout", 
+    <>
+      jumpstart ur blood, 
+      get the oxygen ur brain needs
+    </>, image_importor("dumbell")),
+
+    new DetailOfTips("Structure your day", 
+    <>
+      brain loves certainty, structuring
+      reduces energy of deciding what to do next
+    </>,
+    image_importor("multiple_checkbox")),
+
+
+    new DetailOfTips("Track progress",
+    <>
+      motivate you and keep you
+      in mind how much you progress
+    </>,
+    image_importor("checkbox")),
+
+
+    new DetailOfTips("HEALTHY DIET", 
+    <>
+      your brain need the nutrition and
+      drinking low sugar matcha, water
+      and eating salmon, walnuts,
+    </>, 
+    image_importor("checkbox")),
+
+
+    new DetailOfTips("AUTO GOOGLE",
+      <>
+        This feature auto open the tabs
+        you want to learn immidiately
+        click {<button type="button" onClick={() => {navigator.clipboard.writeText("chrome://settings/onStartup")}}>link</button>} to copy & paste
+        follow the turtorial below
+        
+      </>,
+      image_importor("google", "video"))
+
   ], <Main/>),
+
+
   new TypeOfTips("META-LEARN", [
     new DetailOfTips("HEADER first", "often we don't put much thoughts into headings but putting more processing into it aids u in getting the overall idea better"),
     new DetailOfTips("peripheral vision", "next time you read try not to read each word but in group", image_importor("eye")),
