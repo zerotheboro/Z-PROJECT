1e12d29d22e6708b016e49e4a16a6f1e1898b66a
zerotheboro <2002070343@royal.edu.vn>
2026-01-19 13:22:56 +0700

    benefit update

--- src/HEADER/BENEFIT.jsx
+++ src/HEADER/BENEFIT.jsx
@@ -11,6 +11,7 @@ function Benifit(){
             <li>25+ tips, 1+ must fit u</li>
             <li>summerized from:<br/><a href="https://jamesclear.com/atomic-habits">Atomic Habits</a>, <a href="https://www.youtube.com/@JimKwik">Jim Kwik</a>,...</li>
             <li>these tips boost productivity<br/>therefore less time learning</li>
+            <li>Claim ur bonuses include free<br/>gym California, Boxito 15% off,... </li>
         </ul>
     </div>)
 }
